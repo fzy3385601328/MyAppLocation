68eea7ffb8f45367d69602b33583eebc358ecefa
fzy3385601328 <3385601328@qq.com>
2020-05-08 09:21:39 +0800

    极光推送

---
+++ app/src/main/java/com/example/myappjpush17/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.myappjpush17;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.text.AndroidCharacter;
+
+import com.example.jpushdemo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/myappjpush17/PushService.java
@@ -0,0 +1,8 @@
+package com.example.myappjpush17;
+
+
+import cn.jpush.android.service.JCommonService;
+
+public class PushService extends JCommonService {
+
+}
